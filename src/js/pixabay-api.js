77636f1020b5cb60d3aaee8c634264e20693fb7a
Vladyslava Ishchuk (lala) <ischukvladislava6@gmail.com>
2025-06-15 08:37:03 +0200

    fix: use axios pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,10 +1,24 @@
+import axios from 'axios';
+
 const API_KEY = '50849952-f022a71d57a94af4a04a87620';
 const BASE_URL = 'https://pixabay.com/api/';
 const PER_PAGE = 15;
 
 export async function getImagesByQuery(query, page) {
-  const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${PER_PAGE}`;
-  const response = await fetch(url);
-  if (!response.ok) throw new Error('Network response was not ok');
-  return await response.json();
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page,
+    per_page: PER_PAGE,
+  };
+
+  try {
+    const response = await axios.get(BASE_URL, { params });
+    return response.data;
+  } catch (error) {
+    throw new Error('Failed to fetch images from Pixabay API');
+  }
 }
